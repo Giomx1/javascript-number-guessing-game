ab13406144203bd0138e0d921541e107f44734e6
Giomx1 <105117127+Giomx1@users.noreply.github.com>
2022-09-07 11:58:46 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-let enter = document.getElementById("enterButton");
+let enter = document.getElementById("enterButton");//Grabbing my ids with id method.
 let reset = document.getElementById("resetButton");
 let output = document.getElementById("outputText");
 
-let randomNumber = Math.ceil(Math.random() * 100);
+let randomNumber = Math.ceil(Math.random() * 100);//User must guess a number from 0-100 so a random number must be created every time the game is played.
 
-function checkNumber() {
-  let input = document.getElementById("userInput").value;
-  if (input == randomNumber) {
+function checkNumber() {//function with variable checkNumber.
+  let input = document.getElementById("userInput").value;//using this id method to to later use it to program player input possibilities 
+  if (input == randomNumber) {// Using if methods to display what will be shown if user is right, ,guess too high, guess too low, or invalid entry 
     output.innerHTML = "You guessed right";
     output.style.color = "green";
   }
@@ -25,7 +25,7 @@ function checkNumber() {
     output.innerHTML = "Number cannot be lower than 1";
   }
 }
-enter.addEventListener("click", checkNumber);
-reset.addEventListener("click", function () {
+enter.addEventListener("click", checkNumber);//function will run with "click" event
+reset.addEventListener("click", function () {//page will reload with reset button like a refresh button.
   location.reload();
 });
